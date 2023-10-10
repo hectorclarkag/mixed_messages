56e28022cd3c7dab6a574674dea6294b26557bbb
Heitor <49591729+hectorclarkag@users.noreply.github.com>
2023-10-10 22:26:44 +0100

    packages installed

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+
+
 let mood = ['Good', 'Wonderfull', 'Amazing', 'Perfect']
 let modIndex = Math.floor(Math.random() * mood.length)
 
@@ -9,3 +11,16 @@ let phraseComplement = ['will be warm!', 'Will be freezing today!', 'Today will 
 let phraseComplementIndex = Math.floor(Math.random() * phraseComplement.length)
 
 console.log(mood[modIndex] + ' ' + periodOfTheDay[periodOfTheDayIndex] + ' ' + phraseComplement[phraseComplementIndex])
+
+
+const express = require('express');
+
+app = express();
+
+app.get('/', (req, res) => {
+    res.send(mood[modIndex] + ' ' + periodOfTheDay[periodOfTheDayIndex] + ' ' + phraseComplement[phraseComplementIndex]);
+});
+
+app.listen(3000, () => {
+    console.log('Listening');
+});
