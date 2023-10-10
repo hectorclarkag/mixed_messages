10c513b331fe178b2f7b0725a33de5b4573bfbdf
Heitor <49591729+hectorclarkag@users.noreply.github.com>
2023-10-10 20:51:59 +0100

    ready_basic_program

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let mood = ['Good', 'Wonderfull', 'Amazing', 'Perfect'];
+let modIndex = Math.floor(Math.random() * mood.length);
+
+let periodOfTheDay = ['Morning', 'Afternoon', 'Evening', 'Night'];
+let periodOfTheDayIndex = Math.floor(Math.random() * periodOfTheDay.length);
+
+let phraseComplement = ['will be warm!', 'Will be freezing today!', 'Today will be snowy!', 'is raining again!' ];
+let phraseComplementIndex = Math.floor(Math.random() * phraseComplement.length);
+
+console.log(mood[modIndex] + ' ' + periodOfTheDay[periodOfTheDayIndex] + ' ' + phraseComplement[phraseComplementIndex]);
